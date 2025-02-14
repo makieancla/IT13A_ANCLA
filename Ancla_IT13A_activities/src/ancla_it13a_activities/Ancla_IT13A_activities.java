7934815bee14b7df680057951ea40f13e800bdd8
Mc Francis Estoque Ancla <anclamcfrancis037@gmail.com>
2025-02-14 14:41:28 +0800

    java
    Added Java Application

---
+++ Ancla_IT13A_activities/src/ancla_it13a_activities/Ancla_IT13A_activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ancla_it13a_activities;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ancla_IT13A_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
